2209b08162fc19bac75e760d57b9ff87261cee6f
Maxim <benyachef@gmail.com>
2023-05-08 20:37:23 +0300

    refactor routs

--- src/middlewares/input-validation-middleware.ts
+++ src/middlewares/input-validation-middleware.ts
@@ -1,4 +1,4 @@
-import {validationResult} from "express-validator";
+import {ValidationError, validationResult} from "express-validator";
 import {NextFunction, Request, Response} from "express";
 
 
@@ -10,7 +10,7 @@ export const inputValidationMiddleware = (req: Request, res: Response, next: Nex
     if (!errors.isEmpty()) {
         res.status(400).json({
             errorsMessages: errors.array({onlyFirstError: true})
-                .map(e => {
+                .map((e: ValidationError) => {
                 return {
                     message: e.msg,
                     field: e.type === 'field' ? e.path : null
