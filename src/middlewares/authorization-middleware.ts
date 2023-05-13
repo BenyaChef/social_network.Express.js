1a1a1a433cf332dbe1f45c133324f6cac95f1887
Maxim <benyachef@gmail.com>
2023-05-14 00:25:04 +0300

    debug

--- src/middlewares/authorization-middleware.ts
+++ src/middlewares/authorization-middleware.ts
@@ -1,10 +1,10 @@
 import {NextFunction, Request, Response} from "express";
 
-export const authorizationMiddleware =  (req: Request, res: Response, next: NextFunction) => {
+export const authorizationMiddleware = (req: Request, res: Response, next: NextFunction) => {
     const basic64 = Buffer.from('admin:qwerty').toString('base64')
     const loginData = `Basic ${basic64}`
 
-    if(req.headers.authorization !== loginData) return res.sendStatus(401)
+    if (req.headers.authorization !== loginData) return res.sendStatus(401)
 
-  return  next()
+    return next()
 }
