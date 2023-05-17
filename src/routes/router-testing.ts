206193cb001ab77da622cf98220fe8bf671b6c85
Maxim <benyachef@gmail.com>
2023-05-17 12:32:15 +0300

    e2e tests

--- src/routes/router-testing.ts
+++ src/routes/router-testing.ts
@@ -6,7 +6,9 @@ import {HTTP_STATUS} from "../enum/enum-HTTP-status";
 export const testRouter = Router({})
 
 testRouter.delete('/all-data', async (req: Request, res: Response) => {
-    await blogsCollections.deleteMany({})
-    await postsCollections.deleteMany({})
-    return res.sendStatus(HTTP_STATUS.No_content)
+    const isDelete = await Promise.all([blogsCollections.deleteMany({}), postsCollections.deleteMany({})])
+    if (isDelete) {
+        res.sendStatus(HTTP_STATUS.No_content)
+    }
+    res.sendStatus(HTTP_STATUS.Not_found)
 })
