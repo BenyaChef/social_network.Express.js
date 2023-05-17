b69a8d20218feff952bcca77101914141ca60a18
Maxim <benyachef@gmail.com>
2023-05-17 12:17:24 +0300

    e2e tests

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,14 @@
 import express from 'express';
 import dotenv from 'dotenv'
 import {runDB} from "./db/db";
+import {testRouter} from "./routes/router-testing";
 
 dotenv.config()
 
 export const app = express()
 const port = process.env.PORT || 3003
 
+app.use('/testing', testRouter)
 
 const starApp = async () => {
     await runDB()
--- src/settings.ts
+++ src/settings.ts
@@ -1,5 +1,4 @@
 import express from "express";
-import {testRouter} from "./routes/router-testing";
 import {blogRouter} from "./routes/router-blogs";
 import {postRouter} from "./routes/router-posts";
 
@@ -7,6 +6,6 @@ export const app = express()
 
 app.use(express.json())
 
-app.use('/testing', testRouter)
+
 app.use('/blogs', blogRouter)
 app.use('/posts', postRouter)
