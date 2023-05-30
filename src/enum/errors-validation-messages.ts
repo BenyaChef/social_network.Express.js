4ae9fd4595522c8f11594e75e7dc5d7828956555
Maxim <benyachef@gmail.com>
2023-05-30 18:00:31 +0300

    refactor validation-middlewares.ts

--- src/enum/errors-validation-messages.ts
+++ src/enum/errors-validation-messages.ts
@@ -2,5 +2,6 @@ export enum ERRORS_MESSAGE {
     NOT_EMPTY = 'input must by not empty',
     IS_STRING = 'input must by string',
     IS_LENGTH = 'input length incorrect',
-    PATTERN_INCORRECT = 'unacceptable symbols'
+    PATTERN_INCORRECT = 'unacceptable symbols',
+    SPACES_FIELD = 'field cannot be all spaces'
 }
