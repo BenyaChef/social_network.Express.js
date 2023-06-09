619090090135e3066399151602964c017de93d89
Maxim <benyachef@gmail.com>
2023-06-09 21:01:54 +0300

    confirmation user

--- src/setting.ts
+++ src/setting.ts
@@ -5,15 +5,17 @@ import {postRouter} from "./routes/router-posts";
 import {loginRouter} from "./routes/router-auth";
 import {usersRouter} from "./routes/router-users";
 import {commentsRouter} from "./routes/router-comments";
+import cookieParser from "cookie-parser";
 
 export const app = express()
 app.use(express.json())
+app.use(cookieParser())
 
 app.use('/testing', testRouter)
 
+app.use('/auth', loginRouter)
 app.use('/blogs', blogRouter)
 app.use('/posts', postRouter)
 app.use('/users', usersRouter)
 app.use('/comments', commentsRouter)
 
-app.use('/auth', loginRouter)
--- src/utils/helpers/generate-hash.ts
+++ src/utils/helpers/generate-hash.ts
@@ -3,4 +3,5 @@ import bcrypt from "bcrypt";
 export const generateHash = async (password: string) : Promise<string> => {
     const salt = await bcrypt.genSalt(10)
     return await bcrypt.hash(password, salt)
+
 }
